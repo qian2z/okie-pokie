e8ef5486333f284fd9477e604ff7c49ebfc9065b
qian2z <98630339+qian2z@users.noreply.github.com>
2023-07-11 21:48:11 +0800

    Add Icons

--- src/components/CalorieBar.tsx
+++ src/components/CalorieBar.tsx
@@ -1,4 +1,15 @@
-import { HStack, Stack, Tag, TagLabel, Text } from "@chakra-ui/react";
+import {
+  HStack,
+  Stack,
+  Tag,
+  TagLabel,
+  TagLeftIcon,
+  TagRightIcon,
+  Text,
+} from "@chakra-ui/react";
+import { AiFillFire } from "react-icons/ai";
+import { BiBowlRice } from "react-icons/bi";
+import { MdOutlineEgg, MdWaterDrop } from "react-icons/md";
 
 interface Props {
   calories: string;
@@ -15,24 +26,27 @@ const CalorieBar = ({ calories, carbs, protein, fats }: Props) => {
           <>
             <Tag size="sm" colorScheme="teal">
               <TagLabel>ENERGY</TagLabel>
+              <TagRightIcon boxSize="10px" as={AiFillFire} ml={1} mr={0} />
             </Tag>
             <Text>{calories}</Text>
           </>
         )}
-      </HStack>
-      <HStack mt={1}>
         {carbs !== "" && (
           <>
             <Tag size="sm" colorScheme="purple">
               <TagLabel>CARBS</TagLabel>
+              <TagRightIcon boxSize="10px" as={BiBowlRice} ml={1} mr={0} />
             </Tag>
             <Text>{carbs}</Text>
           </>
         )}
+      </HStack>
+      <HStack mt={1}>
         {protein !== "" && (
           <>
             <Tag size="sm" colorScheme="green">
               <TagLabel>PROTEIN</TagLabel>
+              <TagRightIcon boxSize="10px" as={MdOutlineEgg} ml={1} mr={0} />
             </Tag>
             <Text>{protein}</Text>
           </>
@@ -41,6 +55,7 @@ const CalorieBar = ({ calories, carbs, protein, fats }: Props) => {
           <>
             <Tag size="sm" colorScheme="orange">
               <TagLabel>FATS</TagLabel>
+              <TagRightIcon boxSize="10px" as={MdWaterDrop} ml={1} mr={0} />
             </Tag>
             <Text>{fats}</Text>
           </>
